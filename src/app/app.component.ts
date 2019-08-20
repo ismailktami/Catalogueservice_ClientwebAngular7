dce741c667f4434a6339ad1975a006ef69487305
ismailKtami <ktamiismail@gmail.com>
2019-08-20 02:29:29 +0100

    Add login anf configuration jwt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {AuthenticationService} from "./services/authentication.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'catalogueproduitsweb';
+
+  constructor(private router:Router,private authenticate:AuthenticationService){}
+  logout(){
+    this.authenticate.logout();
+    this.router.navigateByUrl("/login");
+
+  }
+
+  isAuthenticated(){
+     return this.authenticate.isAuthenticated();
+  }
+
+
+  isAdmin(){
+    return this.authenticate.isAdmin();
+  }
+  isUser(){    return this.authenticate.isUser();
+
+
+  }
+ngOnInit(): void {
+    this.authenticate.loadToken();
+
+}
+
 }
